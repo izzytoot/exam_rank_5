3262c9b55954465b677408b78c3fecc8f8e564b6
icunha-t <isabeltootill@gmail.com>
2025-09-25 18:13:11 +0100

    my solution polyset

---
+++ Level 1/polyset/mySolution/searchable_tree_bag.cpp
@@ -0,0 +1,25 @@
+#include "searchable_tree_bag.hpp"
+
+searchable_tree_bag::searchable_tree_bag(){}
+
+searchable_tree_bag::searchable_tree_bag(const searchable_tree_bag& src) : tree_bag(src) {}
+
+searchable_tree_bag& searchable_tree_bag::operator= (const  searchable_tree_bag& src){
+    if (this != &src){
+        tree_bag::operator= (src);
+    }
+    return *this;
+}
+
+searchable_tree_bag::~searchable_tree_bag(){}
+
+bool searchable_tree_bag::has(int n) const{
+    node* curr = tree;
+    if (curr->value == n)
+        return true;
+    else if (n > curr->value)
+        curr = curr->l;
+    else
+        curr = curr->r;
+    return false;
+}
